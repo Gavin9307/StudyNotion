e9ead710c657c4901171efaebb1895dfc72a90ff
gavin9307 <gavindacosta9307@gmail.com>
2024-10-07 22:23:32 +0530

    change tag to category

--- server/models/Course.js
+++ server/models/Course.js
@@ -33,9 +33,9 @@ const courseSchema = new mongoose.Schema({
   price: {
     type: Number,
   },
-  tag: {
+  category: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Tag",
+    ref: "Category",
   },
   studentsEnrolled: [
     {
